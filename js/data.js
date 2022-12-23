fc5e9df4096948ea06a09dee9723fdc8c2599b77
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-23 17:38:02 +0100

    random pictures and profile pictures

--- js/data.js
+++ js/data.js
@@ -2,37 +2,37 @@ const postsData = [
     {
         id: '1',
         name: 'Pippo Baudo',
-        photo: 'https://thispersondoesnotexist.com/',
+        photo: 'img/baudo.webp',
         date: '12-06-2022',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quidem maiores doloremque ad ducimus minus rerum quos',
-        picture: 'https://picsum.photos/seed/picsum/300/200',
+        picture: 'https://picsum.photos/300/200?random=1',
         likes: '115'
     },
     {
         id: '2',
-        name: 'Pippo Baudo',
-        photo: 'https://thispersondoesnotexist.com/',
+        name: 'Paolo Bonolis',
+        photo: 'img/bonolis.jpeg',
         date: '12-06-2022',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quidem maiores doloremque ad ducimus minus rerum quos',
-        picture: 'https://picsum.photos/seed/picsum/300/200',
-        likes: '115'
+        picture: 'https://picsum.photos/300/200?random=2',
+        likes: '85'
     },
     {
         id: '3',
-        name: 'Pippo Baudo',
-        photo: 'https://thispersondoesnotexist.com/',
+        name: 'Madame Curie',
+        photo: 'img/curie.jpg',
         date: '12-06-2022',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quidem maiores doloremque ad ducimus minus rerum quos',
-        picture: 'https://picsum.photos/seed/picsum/300/200',
-        likes: '115'
+        picture: 'https://picsum.photos/300/200?random=3',
+        likes: '78'
     },
     {
         id: '4',
-        name: 'Pippo Baudo',
-        photo: 'https://thispersondoesnotexist.com/',
+        name: 'Margherita Hack',
+        photo: 'img/hack.jpg',
         date: '12-06-2022',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quidem maiores doloremque ad ducimus minus rerum quos',
-        picture: 'https://picsum.photos/seed/picsum/300/200',
-        likes: '115'
+        picture: 'https://picsum.photos/300/200?random=4',
+        likes: '120'
     }
 ]
